109a828df5ebceeaa3d4d552101c7d25835a3c64
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-10 20:11:01 -0600

    "sí"

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 # ./user/serializers.py
+
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
@@ -10,6 +11,12 @@ class UserSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=CustomUser.objects.all())]
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    role = serializers.ChoiceField(
+        choices=CustomUser.Roles.choices,
+        required=False,
+        default=CustomUser.Roles.VIEWER,
+        allow_null=True
+    )
 
     class Meta:
         model = CustomUser
@@ -24,40 +31,27 @@ class UserSerializer(serializers.ModelSerializer):
             'role'
         ]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def validate(self, data):
+        if 'role' not in data or not data['role']:
+            data['role'] = CustomUser.Roles.VIEWER
 
-        # Hacemos role opcional y sin required
-        self.fields['role'] = serializers.ChoiceField(
-            choices=CustomUser.Roles.choices,
-            required=False
-        )
-
-    def validate_role(self, value):
         request = self.context.get('request')
-
-        # Si no se especifica un rol, se asume Viewer (por defecto)
-        if not value:
-            return CustomUser.Roles.VIEWER
-
-        # Si se especifica Admin o Employee y el usuario no es admin autenticado
-        if value in [CustomUser.Roles.ADMIN, CustomUser.Roles.EMPLOYEE]:
+        if data['role'] in [CustomUser.Roles.ADMIN, CustomUser.Roles.EMPLOYEE]:
             if not request or not request.user.is_authenticated or not request.user.is_admin():
-                raise serializers.ValidationError("Solo los administradores pueden asignar este rol.")
+                raise serializers.ValidationError({
+                    'role': "Solo los administradores pueden asignar este rol."
+                })
 
-        return value
+        return data
 
     def create(self, validated_data):
-        # Si no vino rol, se pone Viewer por defecto
-        role = validated_data.get('role', CustomUser.Roles.VIEWER)
-
         user = CustomUser(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             second_last_name=validated_data.get('second_last_name', ''),
-            role=role
+            role=validated_data.get('role', CustomUser.Roles.VIEWER)
         )
         user.set_password(validated_data['password'])
         user.save()
